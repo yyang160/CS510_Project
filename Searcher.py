013755439b1a3e553cd64c16c7232f7fba239928
sherrynorico <sherrynorico@gmail.com>
2019-10-17 20:07:47 -0500

    Metapy Searcher

---
+++ Searcher.py
@@ -0,0 +1,26 @@
+import metapy
+import json
+
+class Searcher:
+
+    def __init__(self):
+        self.idx = metapy.index.make_inverted_index("config.toml")
+
+    def search(self, query):
+
+        with open('docs.json', 'r') as fp:
+            data = json.load(fp)
+
+        results = []
+
+        ranker = metapy.index.OkapiBM25()
+        q = metapy.index.Document()
+        q.content(query)
+        top_docs = ranker.score(self.idx, q, num_results = 10)
+
+        for d_id, score in top_docs:
+            print(d_id, score)
+            if str(d_id) in data:
+                results.append(data[str(d_id)])
+        return results
+
